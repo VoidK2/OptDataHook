54c4b58b584f07db3e6dfc1a37db57e115d8bef1
voidk2 <1399477504@qq.com>
2020-01-21 22:39:20 +0800

    打包发布

--- DataHooker.py
+++ DataHooker.py
@@ -1,13 +1,7 @@
-import os
 import csv
 import subprocess
 
 
-# def load_file(file_name):
-#     result = '.\\dataset\\' + file_name
-#     return result
-
-# -m CE -p Kmedian -f ring.dat
 def load_parameters(file_name):
     result = ' -m CE -p Kmedian -f ' + file_name
     return result
@@ -28,12 +22,12 @@ if __name__ == '__main__':
     for sample_count in range(len(dataset_list)):
         sample_size_now = sample_size_list[sample_count]
         for elite_ratio in range(1, 10):
-            elite_size_now = sample_size_now * elite_ratio * 0.1
+            elite_size_now = int(sample_size_now * elite_ratio * 0.1)
             for smooth_ratio in range(1, 10):
                 smooth_parameter_now = smooth_ratio * 0.1
                 for iteration_ratio in range(1, 101):
                     iteration_number_now = iteration_ratio * 10
-                    full_execute_parameters = 'Opt.exe' + load_parameters(dataset_list[sample_count])
+                    full_execute_parameters = '.\\OptCoreExecutable\\Opt.exe' + load_parameters(dataset_list[sample_count])
                     # print(full_execute_parameters)
                     cmd_process = subprocess.Popen(full_execute_parameters, shell=True,
                                                    stdin=subprocess.PIPE,
@@ -44,14 +38,14 @@ if __name__ == '__main__':
                     cmd_process.stdin.write((str(smooth_parameter_now) + '\n').encode('utf-8'))
                     cmd_process.stdin.write((str(iteration_number_now) + '\n').encode('utf-8'))
                     info, err = cmd_process.communicate()
-                    tmp = info.decode('gbk').split('\r\n')
-                    consume_time = tmp[0]
-                    optimal_value = tmp[1]
+                    print(err.decode('gbk'))
+                    tmp1 = info.decode('gbk').split('\r\n')
+                    consume_time = tmp1[0]
+                    optimal_value = tmp1[1]
+                    print(
+                        str(sample_size_now) + ' ' + str(elite_size_now) + ' ' + str(smooth_parameter_now) + ' ' + str(
+                            iteration_number_now)+ ' '+str(consume_time)+ ' '+str(optimal_value))
                     cnt = cnt+1
                     row_write = [cnt, dataset_list[sample_count], sample_size_now, elite_size_now, smooth_parameter_now,
                                  iteration_number_now, consume_time, optimal_value]
                     write_in_csv(row_write)
-
-
-
-
