832fe76801116d0d8d93d5e53fa07ac4fbd93c7e
voidk2 <1399477504@qq.com>
2020-01-21 19:04:52 +0800

    格式

--- WriteColumnName.py
+++ WriteColumnName.py
@@ -1,9 +1,9 @@
 import csv
 
-
 if __name__ == '__main__':
-    column_name_list = ['id', 'fileName', 'sampleSize', 'eliteSize', 'smoothParameter', 'iterationNumber','consumesTimes', 'optimalValue']
-    testdata = [1,1,1,1,1,1,1,1]
-    with open('.\out\data_backup.csv','a+',newline='') as f:
+    column_name_list = ['id', 'fileName', 'sampleSize', 'eliteSize', 'smoothParameter', 'iterationNumber',
+                        'consumesTimes', 'optimalValue']
+    testData = [1, 1, 1, 1, 1, 1, 1, 1]
+    with open('.\out\data_backup.csv', 'a+', newline='') as f:
         f_csv = csv.writer(f)
         f_csv.writerow(column_name_list)
